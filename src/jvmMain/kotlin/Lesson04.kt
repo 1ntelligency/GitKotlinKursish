416232fcff4a903baed521a63743fa8a6f4bb1f9
mad.lrmk.ru <admin@lrmk.ru>
2023-10-18 13:45:01 +0300

    lessons 1-4

--- src/jvmMain/kotlin/Lesson04.kt
+++ src/jvmMain/kotlin/Lesson04.kt
@@ -2,8 +2,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,7 +21,7 @@ fun LessonJamesBond(dossier: String) {
 /*
     Это задание будет чуть сложнее.
     Создайте столбец (без круглых скобок), внутри него будет 6 элементов:
-    Text заголовка, модификатор выравнивания по центру (Alignment.CenterHorizontally) и отступов на 8, размер шрифта 20
+    Text заголовка, модификатор выравнивания по центру (Alignment.CenterHorizontally) и отступов на 8, размер шрифта 24
     Три горизонтальных ряда с модификатором отступов на 4, внутри каждого два текста:
         левый  с модификатором веса 1: Modifier.weight(1f) и курсивом: fontStyle = FontStyle.Italic
         правый с модификатором веса 3: Modifier.weight(3f) и полужирным: fontWeight = FontWeight.SemiBold
