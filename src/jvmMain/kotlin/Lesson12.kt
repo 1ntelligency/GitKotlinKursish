bff9638daa05892693196eaddb4c45014cad9f51
mad.lrmk.ru <admin@lrmk.ru>
2023-10-18 15:30:31 +0300

    lesson 12

--- src/jvmMain/kotlin/Lesson12.kt
+++ src/jvmMain/kotlin/Lesson12.kt
@@ -14,40 +14,7 @@ import androidx.compose.ui.unit.sp
 
 @Composable    // Используйте exit в качестве параметра-действия для кнопки выхода
 fun LessonRegForm(exit: ()->Unit) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        var login by remember { mutableStateOf("") }
-        var pass by remember { mutableStateOf("") }
-        var gender by remember { mutableStateOf(0) }
-        var rules by remember { mutableStateOf(false) }
-        Text("Регистрация пользователя", fontSize = 20.sp)
-        OutlinedTextField(login, { login = it }, Modifier.padding(4.dp),
-            placeholder = { Text("Введите логин") },
-            label = { Text("Имя пользователя") },
-            singleLine = true
-        )
-        OutlinedTextField(pass, { pass = it }, Modifier.padding(4.dp),
-            placeholder = { Text("Введите не менее 6 букв и цифр") },
-            label = { Text("Пароль") },
-            visualTransformation = PasswordVisualTransformation(),
-            singleLine = true
-        )
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text("Вы:")
-            RadioButton(gender == 1, { gender = 1 })
-            Text("мужчина", Modifier.clickable { gender = 1 })
-            RadioButton(gender == 2, { gender = 2 })
-            Text("женщина", Modifier.clickable { gender = 2 })
-        }
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(rules, { rules = it })
-            Text("с правилами ознакомлен" + if (gender==2) "а" else "  ", Modifier.clickable { rules = !rules })
-        }
-        Button({}) {
-            Icon(Icons.Default.Check, null)
-            Text("Зарегистрироваться")
-        }
-        TextButton(exit) { Text("Выход") }
-    }
+
 }
 
 /*
