42d1435598941723bb0746fc5f3bf7f2685731c1
mad.lrmk.ru <admin@lrmk.ru>
2023-10-18 14:24:39 +0300

    lessons 9-11

--- src/jvmMain/kotlin/Lesson09.kt
+++ src/jvmMain/kotlin/Lesson09.kt
@@ -13,37 +13,7 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun LessonChecks() {
-    Column {
-        var red by remember { mutableStateOf(true) }
-        var yellow by remember { mutableStateOf(true) }
-        var green by remember { mutableStateOf(false) }
-        Row(Modifier.clickable { red = !red }, verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(red, {red = !red})
-            Text("Красный")
-        }
-        Row(Modifier.clickable { yellow = !yellow }, verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(yellow, {yellow = !yellow})
-            Text("Жёлтый")
-        }
-        Row(Modifier.clickable { green = !green }, verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(green, {green = !green})
-            Text("Зелёный")
-        }
-        Column(Modifier.clip(CircleShape).background(Color.Gray)) {
-            Box(
-                Modifier.weight(1f).padding(16.dp).aspectRatio(1f).clip(CircleShape).background(
-                if (red) Color.Red else Color.DarkGray
-            ))
-            Box(
-                Modifier.weight(1f).padding(16.dp).aspectRatio(1f).clip(CircleShape).background(
-                if (yellow) Color.Yellow else Color.DarkGray
-            ))
-            Box(
-                Modifier.weight(1f).padding(16.dp).aspectRatio(1f).clip(CircleShape).background(
-                if (green) Color.Green else Color.DarkGray
-            ))
-        }
-    }
+
 }
 
 /*
